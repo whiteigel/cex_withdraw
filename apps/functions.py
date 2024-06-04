8804e1da496e2d955e7489dea4376e77ff35aa9a
Whiteigel <whiteigel@gmail.com>
2024-06-04 15:17:01 +0200

    Check balance func added

--- apps/functions.py
+++ apps/functions.py
@@ -2,14 +2,60 @@ import ccxt
 
 
 def get_okx_tokens() -> list:
+    """
+    Получение списка торговых пар с биржи OKX
+    :return: список торговых пар
+    """
     okx = ccxt.okx()
-    data = okx.fetch_markets()
-    pairs = [data['symbol'] for data in data]
-    return pairs
+    try:
+        data = okx.fetch_markets()
+        pairs = [data['symbol'] for data in data]
+        return pairs
+    except ccxt.AuthenticationError as e:
+        print("Authentication Error:", e)
+    except Exception as e:
+        print("An error occurred:", e)
 
 
 def get_binance_tokens() -> list:
+    """
+    Получение списка торговых пар с биржи Binance
+    :return: список торговых пар
+    """
     binance = ccxt.binance()
-    data = binance.fetch_markets()
-    pairs = [data['symbol'] for data in data]
-    return pairs
+    try:
+        data = binance.fetch_markets()
+        pairs = [data['symbol'] for data in data]
+        return pairs
+    except ccxt.AuthenticationError as e:
+        print("Authentication Error:", e)
+    except Exception as e:
+        print("An error occurred:", e)
+
+
+def get_balance(apikey: str, secretkey: str, password: str) -> None:
+    """
+    Получение баланса с биржи OKX
+    :param apikey: ваш api ключ
+    :param secretkey: ваш secret ключ
+    :param password: ваш пароль
+    :return: вывод баланса
+    """
+    params = {
+        'type': 'funding'
+    }
+
+    okx = ccxt.okx({
+        'apiKey': apikey,
+        'secret': secretkey,
+        'password': password
+    })
+    try:
+        balance = okx.fetch_balance(params=params)
+        for key, value in balance['total'].items():
+            if value > 0:
+                print(f'Ваш баланс {key}: {value}')
+    except ccxt.AuthenticationError as e:
+        print("Authentication Error:", e)
+    except Exception as e:
+        print("An error occurred:", e)
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from config import apikey, secretkey
-from apps.functions import get_okx_tokens, get_binance_tokens
+from config import apikey, secretkey, password
+from apps.functions import get_okx_tokens, get_binance_tokens, get_balance
 
 
 def main():
     print(f'Торговые пары OKX: {get_okx_tokens()}')
     print('-'*50)
     print(f'Торговые пары Binance: {get_binance_tokens()}')
+    print(get_balance(apikey, secretkey, password))
 
 
 if __name__ == '__main__':
